10ee8d23af6b24d2ebda2f42f397f32290263072
Eli Schiff <elihschiff@gmail.com>
2018-10-01 17:27:40 -0400

    able to edit and display 3d printer setting data

---
+++ public/js/add_settings.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    printerSettings: {}
+  },
+  methods: {
+    uploadSettings: function () {
+      //post the settings to the server
+    }
+  }
+})
