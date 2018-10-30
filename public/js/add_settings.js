b9eb3d01b1314ffec848e5b12f75826dc8331886
Eli Schiff <elihschiff@gmail.com>
2018-10-30 09:29:40 -0400

    Added ability to edit old settings and view them side by side

--- public/js/add_settings.js
+++ public/js/add_settings.js
@@ -1,17 +1,24 @@
 var app = new Vue({
   el: '#app',
   data: {
-    printerSettings: {"test":1}
+    printerSettings: {"test":1},
+    oldPrinterSettings: {"test":1},
+    canEdit: false,
+    oldSettings: null
   },
   mounted:function(){
       //gets settings if there is an id in the query parameters
       this.getSettings();
   },
   methods: {
+    enableEditing: function () {
+      console.log("now enable editing");
+      app.canEdit = true;
+    },
     uploadSettings: function () {
       // $.post( "/add_settings.html", function( data ) {
       // });
-      console.log(JSON.stringify(this.printerSettings));
+      // console.log(JSON.stringify(this.printerSettings));
     },
     //changes the printerSettings to the settings from the database
     getSettings: function(){
@@ -20,6 +27,16 @@ var app = new Vue({
         if(data){
           console.log(JSON.stringify(data[0]));
           app.printerSettings = data[0];
+          app.oldSettings = data[0]._id;
+          delete app.printerSettings._id;
+          delete app.printerSettings.__v;
+          app.printerSettings.parentId = app.oldSettings;
+
+          app.oldPrinterSettings = JSON.parse(JSON.stringify(app.printerSettings));
+
+          if(!app.oldSettings){
+            canEdit = true;
+          }
         }
       });
     }
