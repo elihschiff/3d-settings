668c2a7f9f1b24f2c948e77aa0aa8264d8e64597
Michael <fitzichael@gmail.com>
2018-11-27 14:36:16 -0500

    something something something fix merge

--- server.js
+++ server.js
@@ -83,6 +83,18 @@ app.post('/get_children/:id', (req,res)=> {
   })
 })
 
+//finding stats
+app.post('/get_elements/:id', (req,res)=>{
+  console.log(req.params.id);
+  settingsModel.find({}, (err,elements)=>{
+    let requested_array = [];
+    elements.forEach((elements)=>{
+      requested_array.push(elements[req.params.id]);
+    })
+    res.send(requested_array);
+  })
+});
+
 app.post('/add_settings/:id', (req, res)=>{
   console.log(req.body);
   let instance = new settingsModel({ name: req.body.name, printSpeed: req.body.printSpeed, layerHeight: req.body.layerHeight, plastic: req.body.plastic, parentId : ObjectId(req.params.id)})
